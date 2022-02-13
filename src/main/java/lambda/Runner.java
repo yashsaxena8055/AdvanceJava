a5b22044164d9cf39a1a7680c9f29abdbbce9c5b
Ashish Juyal <ashish.juyal@gmail.com>
2022-02-13 21:02:49 +0530

    lambda

--- src/main/java/lambda/Runner.java
+++ src/main/java/lambda/Runner.java
@@ -6,5 +6,9 @@ public class Runner {
     Person fred = new Person("Fred", "Smith", 56);
     Person maria = new Person("Maria", "Gracia", 43);
     Person rob = new Person("Rob", "Hartman", 36);
+
+    // TODO: Write comparators (firstname and age) for Person using lambda expressions.
+    //  Apply comparators for sorting person list
+    //  When the age is same, it should compare the firstname
   }
 }
