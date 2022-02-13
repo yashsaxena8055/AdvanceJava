9f097294c5f83a590517e016b9219cf300639af6
Ashish Juyal <ashish.juyal@gmail.com>
2022-02-13 09:44:06 +0530

    Adding FirstThread class

---
+++ src/main/java/threads/FirstThread.java
@@ -0,0 +1,12 @@
+package threads;
+
+/**
+ * 1. Use this file to write your first thread program using Runnable interface
+ * 2. When you are done convert the same program by extending the Thread class
+ * */
+public class FirstThread {
+
+  public static void main(String[] args) {
+
+  }
+}
